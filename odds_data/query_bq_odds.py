411fc430400d0e8603fc5b1e48626348b09ee586
Hyungjun Lim <sculd3@gmail.com>
2023-07-16 15:28:37 -0400

    refactor bq query codes and add more functinos

--- odds_data/query_bq_odds.py
+++ odds_data/query_bq_odds.py
@@ -13,27 +13,22 @@ bq_table_id = "odds_hit_recorded"
 # Construct a BigQuery client object.
 bq_client = bigquery.Client()
 
-query = """
+_clause_property_format = 'property = "{property}"'
+_clause_property_all = "TRUE"
+
+_clause_date_between_format = 'game_date >= "{start_date_str}" AND game_date <= "{end_date_str}"'
+_clause_date_2023 = 'game_date >= "2023-04-01"'
+
+_query = """
     SELECT * 
     FROM `trading-290017.major_league_baseball.odds_batter_prop`
-    WHERE property = "{property}" 
-    AND game_date >= "2023-04-01"
+    WHERE {clause_property} 
+    AND {clause_date}
 """
 
-query_all_properties = """
-    SELECT * 
-    FROM `trading-290017.major_league_baseball.odds_batter_prop`
-    WHERE TRUE
-    AND game_date >= "2023-04-01"
-"""
-
-def download_property(property = 'all'):
-    if property == 'all':
-        query_formatted = query_all_properties
-    else:
-        query_formatted = query.format(property=property)
-    print(f'running querry\n{query_formatted}')
-    query_job = bq_client.query(query_formatted)  # Make an API request.
+def read_df_property_query(query):
+    print(f'[read_df_property_query] running querry\n{query}')
+    query_job = bq_client.query(query)  # Make an API request.
 
     row_dicts = []
     for row in query_job:
@@ -41,8 +36,35 @@ def download_property(property = 'all'):
         row_dict = {k: v for k, v in row.items()}
         row_dicts.append(row_dict)
 
-
     df_odds = pd.DataFrame(row_dicts)
-    df_odds['game_id'] = df_odds.game_id.astype(np.int32)
+    if len(df_odds) == 0:
+        return df_odds
 
+    df_odds['game_id'] = df_odds.game_id.astype(np.int32)
+    return df_odds
+
+def read_df_property_between(start_date_str, end_date_str, property = 'all'):
+    if property == 'all':
+        clause_property = _clause_property_all
+    else:
+        clause_property = _clause_property_format.format(property=property)
+
+    clause_date = _clause_date_between_format.format(start_date_str=start_date_str, end_date_str=end_date_str)
+    query_formatted = _query.format(clause_property=clause_property, clause_date = clause_date)
+    return read_df_property_query(query_formatted)
+
+def read_df_property_date(date_str, property = 'all'):
+    return read_df_property_between(date_str, date_str, property=property)
+
+def read_df_property_2023(property = 'all'):
+    if property == 'all':
+        clause_property = _clause_property_all
+    else:
+        clause_property = _clause_property_format.format(property=property)
+
+    query_formatted = _query.format(clause_property=clause_property, clause_date = _clause_date_2023)
+    return read_df_property_query(query_formatted)
+
+def download_property_2023(property = 'all'):
+    df_odds = read_df_property_2023(property = property)
     df_odds.to_pickle(f'odds_data/df_odds_{property}.pkl')
