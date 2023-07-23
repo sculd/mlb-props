b2bf01b0dac396d10eff118ab5a519d4a62e2b15
Hyungjun Lim <sculd3@gmail.com>
2023-07-23 15:08:50 -0400

    add echo statement at the beginning

--- daily_cloud_live_update_run.sh
+++ daily_cloud_live_update_run.sh
@@ -1,4 +1,5 @@
 cd /home/sculd3/projects/mlb-props/
+echo "daily_cloud_live_update_run" >> log.txt
 date >> log.txt
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_today_odds_bq_update.py >> log.txt 2>&1
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/predict_today_matchup.py >> log.txt 2>&1
