708e4e72cdb630f77ba938fc8cbf11fe2e684b53
Hyungjun Lim <sculd3@gmail.com>
2023-07-24 15:57:45 -0400

    add which file runs

--- daily_cloud_live_update_run.sh
+++ daily_cloud_live_update_run.sh
@@ -3,6 +3,9 @@ LOG="logs/log_$(date +%Y-%m-%dT%H:%M:%S%z).txt"
 echo $LOG
 echo "daily_cloud_live_update_run" >> $LOG
 date >> $LOG
+echo "fetch_today_odds_bq_update.py" >> $LOG
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_today_odds_bq_update.py >> $LOG 2>&1
+echo "predict_today_matchup.py" >> $LOG
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/predict_today_matchup.py >> $LOG 2>&1
+echo "read_today_prediction_odds_gcs_update.py" >> $LOG
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/read_today_prediction_odds_gcs_update.py >> $LOG 2>&1
