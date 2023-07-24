dc01f8ee390e3b9b071d4f2b7502f3dea0a3e56a
Hyungjun Lim <sculd3@gmail.com>
2023-07-24 15:37:28 -0400

    write logs to logs/

--- daily_cloud_live_update_run.sh
+++ daily_cloud_live_update_run.sh
@@ -1,6 +1,7 @@
 cd /home/sculd3/projects/mlb-props/
-echo "daily_cloud_live_update_run" >> log.txt
-date >> log.txt
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_today_odds_bq_update.py >> log.txt 2>&1
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/predict_today_matchup.py >> log.txt 2>&1
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/read_today_prediction_odds_gcs_update.py >> log.txt 2>&1
+LOG="logs/log_$(date +%Y-%m-%dT%H:%M:%S%z).txt"
+echo "daily_cloud_live_update_run" >> $LOG
+date >> $LOG
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_today_odds_bq_update.py >> $LOG 2>&1
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/predict_today_matchup.py >> $LOG 2>&1
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/read_today_prediction_odds_gcs_update.py >> $LOG 2>&1
