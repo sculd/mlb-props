6e81731f241ab517266a9c152e45612c03cac56f
Hyungjun Lim <sculd3@gmail.com>
2023-07-25 17:31:13 -0400

    daily_cloud_run.sh daily log

--- daily_cloud_run.sh
+++ daily_cloud_run.sh
@@ -1,4 +1,11 @@
 cd /home/sculd3/projects/mlb-props/
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_boxscores_bq_update.py >> log.txt 2>&1
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_venue_game_temperature_bq_update.py >> log.txt 2>&1
-/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_prediction_db_update.py >> log.txt 2>&1
+LOG="logs/log_daily_$(date +%Y-%m-%dT%H:%M:%S%z).txt"
+echo $LOG
+echo "daily_cloud_run" >> $LOG
+date >> $LOG
+echo "fetch_yesterday_boxscores_bq_update.py" >> $LOG
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_boxscores_bq_update.py >> $LOG 2>&1
+echo "fetch_yesterday_venue_game_temperature_bq_update.py" >> $LOG
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_venue_game_temperature_bq_update.py >> $LOG 2>&1
+echo "fetch_yesterday_prediction_db_update.py" >> $LOG
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_prediction_db_update.py >> $LOG 2>&1
