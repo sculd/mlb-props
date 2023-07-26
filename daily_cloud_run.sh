772c0ae55022d2558c67313057b31b08e41a743b
Hyungjun Lim <sculd3@gmail.com>
2023-07-25 23:47:44 -0400

    daily routine now read / upload history prediction

--- daily_cloud_run.sh
+++ daily_cloud_run.sh
@@ -9,3 +9,5 @@ echo "fetch_yesterday_venue_game_temperature_bq_update.py" >> $LOG
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_venue_game_temperature_bq_update.py >> $LOG 2>&1
 echo "fetch_yesterday_prediction_db_update.py" >> $LOG
 /home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/fetch_yesterday_prediction_db_update.py >> $LOG 2>&1
+echo "read_history_prediction_odds_gcs_update.py" >> $LOG
+/home/sculd3/venvs/mlb/bin/python /home/sculd3/projects/mlb-props/read_history_prediction_odds_gcs_update.py >> $LOG 2>&1
