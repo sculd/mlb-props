7216c8575440ac0f4f069875f160092e8408eb60
Hyungjun Lim <sculd3@gmail.com>
2023-07-25 23:47:12 -0400

    remove data store update for prediction

--- update_data/live_prediction.py
+++ update_data/live_prediction.py
@@ -13,7 +13,6 @@ import model.common, model.odds_eval
 
 import pycaret
 from pycaret import classification
-from google.cloud import datastore
 from google.cloud import bigquery
 import update_data.common
 
@@ -25,50 +24,12 @@ bq_table_id = "live_prediction_batter_prop"
 bq_table_full_id = f'{gcp_project_id}.{bq_dataset_id}.{bq_table_id}'
 ds_entity_type = "MLBBatterPropPredictionOdds"
 
-_ds_client = datastore.Client()
 _sd_write_size_batch = 30
 
 _regression_model_1hits = pycaret.classification.load_model(model.common.model_1hits_file_name)
 _regression_model_1hstrikeouts = pycaret.classification.load_model(model.common.model_1hstrikeouts_file_name)
 
 _bq_client = bigquery.Client()
-
-
-def write_df_prediction_datastore(df_prediction_odds, property_column_name):
-    entities = []
-    for _, prediction_odds in df_prediction_odds.iterrows():
-        key = _ds_client.key(ds_entity_type)
-        entity = datastore.Entity(key)
-        entity_properties = \
-            {
-                "date": prediction_odds.game_date,
-                "date_str": str(prediction_odds.game_date.date()),
-
-                "game_venue": prediction_odds.game_venue,
-                "game_id": prediction_odds.game_id,
-                "pitching_name": prediction_odds.pitching_name,
-                "pitching_id": prediction_odds.pitching_id,
-                "batting_name": prediction_odds.batting_name,
-                "batting_id": prediction_odds.batting_id,
-
-                "property_name": property_column_name,
-
-                "prediction_label": prediction_odds.prediction_label,
-                "prediction_score": prediction_odds.prediction_score,
-                "theo_odds": prediction_odds.theo_odds,
-            }
-
-        entity.update(entity_properties)
-        entities.append(entity)
-
-    if len(entities) == 0:
-        return
-
-    n_batches = math.ceil(len(entities) / _sd_write_size_batch)
-    entities_batches = np.array_split(entities, n_batches)
-    for entities_batch in entities_batches:
-        entities_batch = list(entities_batch)
-        _ds_client.put_multi(entities_batch)
 
 
 def write_df_prediction_local_temp(df_prediction, property_column_name):
@@ -131,21 +92,6 @@ def write_df_prediction_bq(df_prediction, property_column_name):
         json_file_name, bq_table_full_id, schema, rewrite=True
     )
 
-
-def update_prediction_datastore_between(start_date_str, end_date_str):
-    print(f'update_prediction_odds_datastore_between {start_date_str} {end_date_str}')
-    schedules = collect_data.schedules.fetch_schedule_between(start_date_str, end_date_str)
-    print(f'schedules: {len(schedules)}')
-
-    df_game_matchup = collect_data.game_matchup.get_df_game_between(start_date_str, end_date_str)
-
-    df_prediction_1hits = model.odds_eval.df_prediction_add_odd(df_game_matchup[['game_id'] + model.common.features_1hits_recorded + [model.common.target_1hits_recorded]], _regression_model_1hits)
-    write_df_prediction_datastore(df_prediction_1hits, "batting_1hits_recorded")
-
-    df_prediction_1strikeouts = model.odds_eval.df_prediction_add_odd(df_game_matchup[['game_id'] + model.common.features_1hstrikeouts_recorded + [model.common.target_1hstrikeouts_recorded]], _regression_model_1hstrikeouts)
-    write_df_prediction_datastore(df_prediction_1strikeouts, "batting_1strikeOuts_recorded")
-
-
 def update_prediction_bq_between(start_date_str, end_date_str):
     print(f'update_prediction_bq_between {start_date_str} {end_date_str}')
     schedules = collect_data.schedules.fetch_schedule_between(start_date_str, end_date_str)
@@ -162,7 +108,6 @@ def update_prediction_bq_between(start_date_str, end_date_str):
 
 def update_prediction_db_between(start_date_str, end_date_str):
     update_prediction_bq_between(start_date_str, end_date_str)
-    update_prediction_datastore_between(start_date_str, end_date_str)
 
 def update_prediction_db_ndays_prior(days):
     date_ndays_prior = (datetime.datetime.now(pytz.timezone('US/Eastern')) - datetime.timedelta(days=days)).strftime("%Y-%m-%d")
@@ -199,7 +144,7 @@ def read_df_live_prediction_bq_between(start_date_str, end_date_str):
     if len(df_live_prediction) == 0:
         print('df_live_prediction is empty')
         return df_live_prediction
-    
+
     dedupe_keys = ["game_id", "pitching_name", "batting_name", "property_name"]
     df_live_prediction = df_live_prediction.sort_values(dedupe_keys + ["ingestion_datetime"]).drop_duplicates(dedupe_keys, keep='last')
     print(f'done read_rediction_bq_between {start_date_str} to {end_date_str}')
--- update_data/prediction.py
+++ update_data/prediction.py
@@ -13,7 +13,6 @@ import model.common, model.odds_eval
 
 import pycaret
 from pycaret import classification
-from google.cloud import datastore
 from google.cloud import bigquery
 import update_data.common
 
@@ -24,62 +23,12 @@ bq_dataset_id = "major_league_baseball"
 bq_table_id = "prediction_batter_prop"
 bq_table_full_id = f'{gcp_project_id}.{bq_dataset_id}.{bq_table_id}'
 
-_ds_client = datastore.Client()
 _sd_write_size_batch = 30
 
 _regression_model_1hits = pycaret.classification.load_model(model.common.model_1hits_file_name)
 _regression_model_1hstrikeouts = pycaret.classification.load_model(model.common.model_1hstrikeouts_file_name)
 
 _bq_client = bigquery.Client()
-
-def write_df_prediction_odds_datastore(df_prediction_odds, property_column_name):
-    entities = []
-    for _, prediction_odds in df_prediction_odds.iterrows():
-        key = _ds_client.key("MLBBatterPropPredictionOdds")
-        entity = datastore.Entity(key)
-        entity_properties = \
-            {
-                "date": prediction_odds.game_date,
-                "date_str": str(prediction_odds.game_date.date()),
-
-                "game_venue": prediction_odds.game_venue,
-                "game_id": prediction_odds.game_id,
-                "pitching_name": prediction_odds.pitching_name,
-                "pitching_id": prediction_odds.pitching_id,
-                "batting_name": prediction_odds.batting_name,
-                "batting_id": prediction_odds.batting_id,
-
-                "property_name": property_column_name,
-                "property_value": prediction_odds[property_column_name],
-
-                "prediction_label": prediction_odds.prediction_label,
-                "prediction_score": prediction_odds.prediction_score,
-                "theo_odds": prediction_odds.theo_odds,
-            }
-        if 'over_prob' in prediction_odds:
-            entity_odds_properties = {
-                "team_home": prediction_odds.team_home,
-                "team_away": prediction_odds.team_away,
-                "over_prob": prediction_odds.over_prob,
-                "over_line": prediction_odds.over_line,
-                "over_odds": prediction_odds.over_odds,
-                "under_prob": prediction_odds.under_prob,
-                "under_line": prediction_odds.under_line,
-                "under_odds": prediction_odds.under_odds,
-            }
-            entity_properties = {**entity_properties, **entity_odds_properties}
-
-        entity.update(entity_properties)
-        entities.append(entity)
-
-    if len(entities) == 0:
-        return
-
-    n_batches = math.ceil(len(entities) / _sd_write_size_batch)
-    entities_batches = np.array_split(entities, n_batches)
-    for entities_batch in entities_batches:
-        entities_batch = list(entities_batch)
-        _ds_client.put_multi(entities_batch)
 
 
 def write_df_prediction_local_temp(df_prediction, property_column_name):
@@ -106,6 +55,8 @@ def write_df_prediction_local_temp(df_prediction, property_column_name):
                     "prediction_label": prediction.prediction_label,
                     "prediction_score": prediction.prediction_score,
                     "theo_odds": prediction.theo_odds,
+
+                    "ingestion_datetime": datetime.datetime.now(),
                 }
 
             jf.write(json.dumps(payload, cls=update_data.common.NpEncoder) + '\n')
@@ -129,34 +80,11 @@ def write_df_prediction_odds_bq(df_prediction, property_column_name):
             bigquery.SchemaField("prediction_label", "INTEGER", "REQUIRED"),
             bigquery.SchemaField("prediction_score", "FLOAT", "REQUIRED"),
             bigquery.SchemaField("theo_odds", "INTEGER", "REQUIRED"),
+            bigquery.SchemaField("ingestion_datetime", "DATETIME"),
         ]
     update_data.common.upload_newline_delimited_json_file_to_gcs_then_import_bq(
         json_file_name, bq_table_full_id, schema,
     )
-
-
-def update_prediction_odds_datastore_between(start_date_str, end_date_str):
-    print(f'update_prediction_odds_datastore_between {start_date_str} {end_date_str}')
-    schedules = collect_data.schedules.fetch_schedule_between(start_date_str, end_date_str)
-    print(f'schedules: {len(schedules)}')
-
-    df_game_matchup = collect_data.game_matchup.get_df_game_between(start_date_str, end_date_str)
-    df_odds = odds_data.query_bq_odds.read_df_property_between(start_date_str, end_date_str)
-
-    df_prediction_1hits = model.odds_eval.df_prediction_add_odd(df_game_matchup[['game_id'] + model.common.features_1hits_recorded + [model.common.target_1hits_recorded]], _regression_model_1hits)
-    if len(df_odds) == 0:
-        df_prediction_odds_1hits = df_prediction_1hits
-    else:
-        df_prediction_odds_1hits = model.odds_eval.merge_df_prediction_over_odds(df_prediction_1hits, df_odds[df_odds.property == 'Hits'], model.common.target_1hits_recorded, 1.0)
-    write_df_prediction_odds_datastore(df_prediction_odds_1hits, "batting_1hits_recorded")
-
-    df_prediction_1strikeouts = model.odds_eval.df_prediction_add_odd(df_game_matchup[['game_id'] + model.common.features_1hstrikeouts_recorded + [model.common.target_1hstrikeouts_recorded]], _regression_model_1hstrikeouts)
-    if len(df_odds) == 0:
-        df_prediction_odds_1strikeouts = df_prediction_1strikeouts
-    else:
-        df_prediction_odds_1strikeouts = model.odds_eval.merge_df_prediction_over_odds(df_prediction_1strikeouts, df_odds[df_odds.property == 'Strikeouts'], model.common.target_1hstrikeouts_recorded, 1.0)
-    write_df_prediction_odds_datastore(df_prediction_odds_1strikeouts, "batting_1strikeOuts_recorded")
-
 
 def update_prediction_bq_between(start_date_str, end_date_str):
     print(f'update_prediction_bq_between {start_date_str} {end_date_str}')
@@ -174,7 +102,6 @@ def update_prediction_bq_between(start_date_str, end_date_str):
 
 def update_prediction_db_between(start_date_str, end_date_str):
     update_prediction_bq_between(start_date_str, end_date_str)
-    update_prediction_odds_datastore_between(start_date_str, end_date_str)
 
 def update_prediction_db_ndays_prior(days):
     date_ndays_prior = (datetime.datetime.now(pytz.timezone('US/Eastern')) - datetime.timedelta(days=days)).strftime("%Y-%m-%d")
@@ -204,10 +131,13 @@ def read_df_prediction_bq_between(start_date_str, end_date_str):
         row_dict = {k: v for k, v in row.items()}
         row_dicts.append(row_dict)
 
-    df_live_prediction = pd.DataFrame(row_dicts)
+    df_prediction = pd.DataFrame(row_dicts)
+    df_prediction = df_prediction.rename(columns={'date': 'game_date'})
+    df_prediction['game_date'] = pd.to_datetime(df_prediction['game_date'])
+    df_prediction['game_id'] = df_prediction.game_id.astype(np.int32)
     print(f'done read_rediction_bq_between {start_date_str} to {end_date_str}')
 
-    return df_live_prediction
+    return df_prediction
 
 def read_df_prediction_bq_today():
     print(f'read_df_prediction_bq_today')
