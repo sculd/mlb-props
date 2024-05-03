ed751c8ec445f2943c0c2423083d78afbc26fbb9
Hyungjun Lim <sculd3@gmail.com>
2024-05-02 22:31:01 -0400

    read_history_prediction_odds_gcs_update.py update

--- read_history_prediction_odds_gcs_update.py
+++ read_history_prediction_odds_gcs_update.py
@@ -7,8 +7,10 @@ if os.path.exists('credential.json'):
 
 import odds_data.query_bq_odds
 
+year = 2024
+print('updating the odds')
 property = "all"
-df_history_odds = odds_data.query_bq_odds.read_df_property_2023(property=property)
+df_history_odds = odds_data.query_bq_odds.download_df_property_year(year, property=property)
 df_history_odds_hits = df_history_odds[(df_history_odds.property=="Hits") ]
 df_history_odds_strikeouts = df_history_odds[(df_history_odds.property=="Strikeouts")]
 
@@ -21,7 +23,8 @@ update_data.common.upload_file_to_public_gcs(pkl_file_name_odds_strikeouts, pkl_
 
 import update_data.prediction
 
-df_history_prediction = update_data.prediction.read_df_prediction_bq_2023()
+print('updating the preduction')
+df_history_prediction = update_data.prediction.read_df_prediction_bq_year(year)
 df_history_prediction_1hits = df_history_prediction[df_history_prediction.property_name == "batting_1hits_recorded"]
 df_history_prediction_2hits = df_history_prediction[df_history_prediction.property_name == "batting_2hits_recorded"]
 df_history_prediction_1strikeouts = df_history_prediction[df_history_prediction.property_name == "batting_1strikeOuts_recorded"]
